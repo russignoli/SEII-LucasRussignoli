c143afdd960fb82e50ab5bcd89e28214c2bb3b97
Lucas Russignoli Peixoto <38055776+russignoli@users.noreply.github.com>
2022-06-23 22:38:20 -0300

    sem07

--- semana07/problema02/SayHello.py
+++ semana07/problema02/SayHello.py
@@ -1,9 +1,11 @@
+#import libraries
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+# --
 
 class SayHello(App):
     def build(self):
